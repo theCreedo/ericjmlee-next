137401f2435f1a12c26e837a350639a313d7cc41
Eric Lee <hownl96@gmail.com>
2020-08-10 23:43:14 -0500

    Add 404 error page

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 export default function Custom404() {
     return (
         <Layout>
-            <h1>404 - Page Not Found</h1>
+            <h1>404 - Page Not Available Now</h1>
         </Layout>
     )
 }
