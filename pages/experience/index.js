a960241138db3939a1cc3f50ff30a0409b731d29
Eric Lee <hownl96@gmail.com>
2020-08-18 17:51:50 -0500

    feat: populate /experience page with work experience

--- pages/experience/index.js
+++ pages/experience/index.js
@@ -1,18 +1,19 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../../components/layout'
 import utilStyles from '../../styles/utils.module.css'
-import { getSortedPostsData } from '../../lib/posts'
+import { getSortedExperiencesData } from '../../lib/experiences'
+import Date from '../../components/date'
 
 export async function getStaticProps() {
-    const allPostsData = getSortedPostsData()
+    const allExperiencesData = getSortedExperiencesData()
     return {
         props: {
-            allPostsData
+            allExperiencesData
         }
     }
 }
 
-export default function Experience({ allPostsData }) {
+export default function Experience({ allExperiencesData }) {
     return (
         <Layout experience>
             <Head>
@@ -21,14 +22,15 @@ export default function Experience({ allPostsData }) {
             <section className={`${utilStyles.divContainer} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Experience</h2>
                 <ul className={utilStyles.list}>
-                    {/* {allPostsData.map(({ id, date, title }) => (
+                    {allExperiencesData.map(({ id, start_date, end_date, location, job_title, link, title }) => (
                         <li className={utilStyles.listItem} key={id}>
-                            <Link href="/posts/[id]" as={`/posts/${id}`}>
-                                <a>{title}</a>
-                            </Link>
-                            <br />
+                            <h1 className={utilStyles.headingMd}><a href={link}>{title}</a> @ {location}</h1>
+                            <small className={utilStyles.lightText}>
+                                <p>{job_title}</p>
+                                <Date dateString={start_date} /> to <Date dateString={end_date} />
+                            </small>
                         </li>
-                    ))} */}
+                    ))}
                 </ul>
             </section>
         </Layout >
