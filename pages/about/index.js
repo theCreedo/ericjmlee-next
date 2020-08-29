73a06f4bc4c05304aeec004c504fcad2762763b5
Eric Lee <hownl96@gmail.com>
2020-08-29 01:27:34 -0500

    feat: add myers briggs and enneagram to about

--- pages/about/index.js
+++ pages/about/index.js
@@ -28,11 +28,14 @@ export default function About({ allPostsData }) {
                 <p>I’ve lived in Texas all my life and have a totally normal amount of Dallas pride ✊, but Taiwan will always be special to me because it symbolizes my heritage and family 🇹🇼. Michigan also holds a piece of my heart since my favorite mission trips were at Ann Arbor and Detroit.</p>
                 <p>Currently I work in ATX as a Software Engineer @ <a href='https://www.retailmenot.com/'>RetailMeNot</a>, working on full stack web development.</p>
                 <p>In my free time, I serve in my church called <a href="https://austin.hmcc.net/">HMCC Austin</a>, work on my personal newsletter <a href="https://ericlee.substack.com/">Savvy Saturdays</a>, and provide consulting & mentorship for startups & creative ventures like YouTube, Etsy, & Instagram.</p>
+                <p>My Myers-Briggs is <a href="https://www.16personalities.com/enfp-personality">ENFP</a>, and my Enneagram type is <a href="https://www.crystalknows.com/enneagram/type-2-wing-3">2w3</a>.</p>
+
                 <h2 className={utilStyles.headingLg}>Interests</h2>
                 <p>My hobbies include biking, writing, reading webtoons & manga, consuming a lot of YouTube, playing basketball, and perusing newsletters as well as stress baking, practicing guitar, working out, and enjoying the occasional video game.</p>
                 <p>I'm not too much of a travel freak since it takes up a lot of energy to explore, but friends can usually coerce me into a trip. In general, I like to tell people that as long as I have internet and a laptop, I can be entertained for a while.</p>
+
                 <h2 className={utilStyles.headingLg}>Contact Me</h2>
-                <p>I love meeting new people. Sharing about life, helping people on their startups and creative ventures, as well as providing (or receiving) perspective are all life's joys.</p>
+                <p>I love meeting new people! Sharing about life, helping people on their startups and creative ventures, as well as providing (or receiving) perspective are all life's joys.</p>
                 <p>Please reach out if you want to chat! Email me @ <a href="mailto:heyericjmlee@gmail.com">heyericjmlee@gmail.com</a> or message me through any social media.</p>
                 <Link href='/'><a>← Back to Home</a></Link>
             </section>
