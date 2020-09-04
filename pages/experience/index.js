27b9eaedfd006e048385a1231089a431ab8ecef0
Eric Lee <hownl96@gmail.com>
2020-09-04 15:28:49 -0500

    fix: add spacing in /experience

--- pages/experience/index.js
+++ pages/experience/index.js
@@ -22,6 +22,7 @@ export default function Experience({ allExperiencesData }) {
             </Head>
             <section className={`${utilStyles.divContainer} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Experience</h2>
+                <br />
                 <ul className={utilStyles.list}>
                     {allExperiencesData.map(({ id, title, start_date, end_date, location, job_title, company_url, logo_url, current }) => (
                         <li className={utilStyles.listItem} key={id}>
@@ -33,6 +34,9 @@ export default function Experience({ allExperiencesData }) {
                         </li>
                     ))}
                 </ul>
+                {/* TODO ADD SKILLS */}
+                {/* <p>Currently a full stack web developer in React that gets dirty in PHP & Java at times.</p>
+                <p>Worked in Ruby on Rails, Scala, Flask, & Java.</p> */}
                 <Link href='/'><a>← Back to Home</a></Link>
             </section>
         </Layout >
