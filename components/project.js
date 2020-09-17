9644652ee5760902ea20630ac9382400708f6e54
Eric Lee <hownl96@gmail.com>
2020-09-16 21:30:00 -0500

    feat: add projectItem component

---
+++ components/project.js
@@ -0,0 +1,31 @@
+import utilStyles from '../styles/utils.module.css'
+import Date from './date'
+
+export function YoutubeItem({ youtube_embed_link }) {
+    return (<div className={utilStyles.iframeVideo}>
+        <iframe width="560" height="315" src={youtube_embed_link} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>)
+}
+
+export default function ProjectItem({ id, start_date, title, one_liner, tech, header_link, youtube_embed_link, image_link, image_alt_text }) {
+    return (<li className={utilStyles.listItem} key={id}>
+        <div>
+            {youtube_embed_link ?
+                <YoutubeItem youtube_embed_link={youtube_embed_link} /> :
+                (<a href={header_link}>
+                    <img className={utilStyles.gridImage} src={image_link} alt={image_alt_text} />
+                </a>)}
+            <h4 className={utilStyles.headingLgProjects} >
+                <a href={header_link}>
+                    {title}
+                </a>
+            </h4>
+            <small className={utilStyles.lightText}>
+                <Date dateString={start_date} isPost={false} />
+            </small>
+        </div>
+        <>{one_liner}</>
+        <br />
+        <small><p>Used: <i>{tech}</i></p></small>
+    </li>);
+}
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -4,6 +4,7 @@ import Layout, { siteTitle } from '../../components/layout'
 import utilStyles from '../../styles/utils.module.css'
 import { getSortedProjectsData } from '../../lib/projects'
 import Date from '../../components/date'
+import ProjectItem from '../../components/project'
 
 export async function getStaticProps() {
     const allProjectsData = getSortedProjectsData()
@@ -23,29 +24,8 @@ export default function Project({ allProjectsData }) {
             <section className={`${utilStyles.divContainer} ${utilStyles.padding1px}`}>
                 <br />
                 <ul className={utilStyles.list}>
-                    {allProjectsData.map(({ id, start_date, title, one_liner, tech, header_link, youtube_embed_link, image_link, image_alt_text }) => (
-                        <li className={utilStyles.listItem} key={id}>
-                            <div>
-
-                                {youtube_embed_link ?
-                                    (<div className={utilStyles.iframeVideo}>
-                                        <iframe width="560" height="315" src={youtube_embed_link} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                    </div>) :
-                                    (<a href={header_link}><img src={image_link} alt={image_alt_text}></img></a>
-                                    )}
-                                <h4 className={utilStyles.headingLgProjects} >
-                                    <a href={header_link}>
-                                        {title}
-                                    </a>
-                                </h4>
-                                <small className={utilStyles.lightText}>
-                                    <Date dateString={start_date} isPost={false} />
-                                </small>
-                            </div>
-                            <>{one_liner}</>
-                            <br />
-                            <small><p>Used: <i>{tech}</i></p></small>
-                        </li>
+                    {allProjectsData.map((projectData) => (
+                        <ProjectItem {...projectData} />
                     ))}
                 </ul>
                 <Link href='/'><a>← Back to Home</a></Link>
