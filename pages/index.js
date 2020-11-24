1706bcf57b6f8484b17da1bd7e5fb39e2988734a
Eric Lee <hownl96@gmail.com>
2020-11-24 02:18:42 -0600

    feat: update front page bio

--- pages/index.js
+++ pages/index.js
@@ -22,14 +22,15 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.divContainer}>
         <br></br>
         <blockquote><b><i>Learning is my passion. Empowering is my mission.</i></b></blockquote>
+        <p>Living out my faith, exemplifying leadership, and hustling to be better, I aspire to be 1% better every day by creating & impacting wherever I can.</p>
         <p>Currently I work in ATX as a Software Engineer @ <a href='https://www.retailmenot.com/'>RetailMeNot</a>, working on full stack web development.</p>
         <p>In my free time, I focus on:</p>
         <ul>
+          <li>Working on my <a href="https://ericlee.substack.com/">weekly newsletter</a> on my learnings, faith, leadership, & productivity.</li>
+          <li>Writing thoughts out on faith, struggles, & life through my <a href='/blog'>blog</a>.</li>
           <li>Serving in my church, <a href="https://austin.hmcc.net/">HMCC Austin</a>, as a leader.</li>
-          <li>Designing and building new features for this website.</li>
-          <li>Writing thoughts out on faith, struggles, & life.</li>
-          <li>Working on <a href="https://ericlee.substack.com/">Savvy Saturdays</a>, a weekly newsletter on my learnings, faith, leadership, & productivity.</li>
-          <li>Consulting & mentoring startups & creative ventures like YouTube, Etsy, & Instagram.</li>
+          <li>Designing and building <a href="https://github.com/theCreedo/nextjs-blog/issues">new features</a> for this website.</li>
+          <li>Collaborating with college students and entrepreneurs on their creative ventures (YouTube, Instagram, startups).</li>
         </ul>
         {/* TODO: add latest blog post */}
         <NewsletterForm />
