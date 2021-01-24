668e1dea64efccd150c503a173012f6780d52ea0
Eric Lee <hownl96@gmail.com>
2021-01-23 22:13:51 -0600

    feat: update newsletter details to match substack website

--- components/newsletter.js
+++ components/newsletter.js
@@ -5,7 +5,7 @@ export default function NewsletterForm() {
         <div className={utilStyles.newsletterForm}>
             <h2 className={utilStyles.headingLg}><b>💌 Join the "Savvy Saturdays" Mailing List</b></h2>
             <div className="">
-                <p>Every Saturday, I send out an issue on practical learnings, a curated collection of articles, and tidbits of verses & quotes to chew on.</p>
+                <p>Every Saturday, I send out an issue on self-improvement, a curated collection of insightful fun resources, and tidbits of learnings, verses & quotes to chew on.</p>
                 <form action="https://ericlee.substack.com/api/v1/free?nojs=true" method="post" className="form " noValidate="">
                     <input type="hidden" name="first_url" value="https://ericlee.substack.com/" />
                     <input type="hidden" name="first_referrer" value="https://substack.com/signup?oauth_token=1-MpKwAAAAAA8W84AAABb-6Z3GY" />
--- pages/newsletter/index.js
+++ pages/newsletter/index.js
@@ -13,11 +13,10 @@ export default function Newsletter() {
             </Head>
             <section className={`${utilStyles.divContainer} ${utilStyles.padding1px}`}>
                 <br />
-                <p>Hey there!</p>
-                <p>As a Christian, I've struggled to find ways to live out my faith while doing good work effectively & efficiently. I've learned that being a light requires being equipped well, both spiritually and practically.</p>
-                <p>Perusing newsletters, I couldn't find any that addressed these things well, so I decided to make my own.</p>
-                <p><a href="https://ericlee.substack.com">Savvy Saturdays</a> is my weekly email newsletter where I share learnings, a curated collection of articles on faith, leadership, & productivity, as well as tidbits of thoughts, verses, and quotes to chew on.</p>
-                <p>If you want to learn and be inspired practically, spiritually, effectively, or efficiently, sign up for my free newsletter below!</p>
+                <p>I consume a lot of content and love to share my findings with others. Whether it’s inspiring articles, deep analytical books, compelling stories, (or even hilarious memes), I find my learnings have been timely for others.</p>
+                <p>Many newsletters I followed that impacted my life shared in a similar way I did (except in a scaled manner).</p>
+                <p>Inspired, I decided to create my own.</p>
+                <p>If you want to learn, grow, and be inspired to be savvier each week, feel free to sign up for the newsletter below!</p>
                 <NewsletterForm />
                 <br />
                 <p>(<a href="https://ericlee.substack.com/feed">RSS Feed</a>)</p>
