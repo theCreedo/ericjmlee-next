7a859eff86fb07480b1fc7534303317c1eeae750
Eric Lee <hownl96@gmail.com>
2021-04-26 22:34:42 -0500

    feat: add updated newsletter description

--- pages/newsletter/index.js
+++ pages/newsletter/index.js
@@ -14,9 +14,9 @@ export default function Newsletter() {
             <section className={`${utilStyles.divContainer} ${utilStyles.padding1px}`}>
                 <br />
                 <p>I consume a lot of content and love to share my findings with others. Whether it’s inspiring articles, deep analytical books, compelling stories, (or even hilarious memes), I find my learnings have been timely for others.</p>
-                <p>Many newsletters I followed that impacted my life shared in a similar way I did (except in a scaled manner).</p>
-                <p>Inspired, I decided to create my own.</p>
-                <p>If you want to learn, grow, and be inspired to be savvier each week, feel free to sign up for the newsletter below!</p>
+                <p>Many newsletters I followed that impacted my life shared in a similar way I did - in a scaled manner.</p>
+                <p>Inspired, I decided to create my own newsletter to scale my impact on others too.</p>
+                <p>If you want to learn in 5 mins a savvy concept and grow alongside other curious people, join below! (here's an <a href="https://ericlee.substack.com/archive">archive</a>  if you're interested in a taste of the content.)</p>
                 <NewsletterForm />
                 <br />
                 <p>(<a href="https://ericlee.substack.com/feed">RSS Feed</a>)</p>
