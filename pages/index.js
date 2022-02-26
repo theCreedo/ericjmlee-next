0b08e4d2e9ddd1d740041ea3c2d4d0222cf34e5e
Eric Lee <hownl96@gmail.com>
2022-02-26 17:01:26 -0600

    feat: update front page items

--- pages/index.js
+++ pages/index.js
@@ -40,13 +40,16 @@ export default function Home({ allPostsData }) {
         </ul>
         <p>In my free time, I focus on:</p>
         <ul>
-          <li>Preparing to launch a <a href="https://twitter.com/leveluplee">podcast on leveling up</a>.</li>
+          <li>Co-leading Product & Marketing for <a href="https://fiesta.community/">FIESTA</a>, an Austin tech community.</li>
+          <li>Helping with content strategy for <a href="https://idserve.us/">Indigitous Serve</a>.</li>
+          <li>Meeting cool ambitious people on <a href="https://lunchclub.com/?invite_code=ericl39">Lunchclub.ai</a>.</li>
+          <li>Playing a trading card game, <a href="https://fabtcg.com/">Flesh and Blood</a>.</li>
+          <li>Playing video games (recently <a href="https://www.megacrit.com/">Slay the Spire</a>).</li>
+          <li>Serving in my church, <a href="https://austin.hmcc.net/">HMCC Austin</a>.</li>
+          <li>Writing faith thoughts and other ponderings on <a href='https://thecreedo.medium.com'>Medium</a>.</li>
           <li>Exercising & listening to Audible books (tracked on <Link href="https://www.goodreads.com/user/show/127464751-eric-lee">GoodReads</Link>).</li>
           <li>Working on my <a href="https://ericlee.substack.com/">newsletter</a>, sharing practical learnings & resources weekly.</li>
-          <li>Writing faith thoughts and other ponderings on <a href='https://thecreedo.medium.com'>Medium</a>.</li>
-          <li>Serving in my church, <a href="https://austin.hmcc.net/">HMCC Austin</a>.</li>
           <li>Designing & building <a href="https://github.com/theCreedo/ericjmlee-next/issues">new features</a> for this website when in the coding zone.</li>
-          <li>Meeting cool ambitious people on <a href="https://lunchclub.com/?invite_code=ericl39">Lunchclub.ai</a>.</li>
         </ul>
         <NewsletterForm />
       </section>
